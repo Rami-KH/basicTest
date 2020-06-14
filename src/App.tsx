eb2cab55e8cb75a9287465fa5aeaf881e6e62671
Rami Keski-Honkola <rami.keski-honkola@kompozure.com>
2020-06-15 00:06:53 +0300

    index text changed

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,9 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <p>
+          Commit changes and push to master and magic happens ;)
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
